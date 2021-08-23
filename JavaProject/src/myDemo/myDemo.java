ee1e74f00194f8c31610768746b490875b193146
hund1235 <hhfanta21@gmail.com>
2021-08-23 14:50:17 -0400

    my new commit

--- JavaProject/src/myDemo/myDemo.java
+++ JavaProject/src/myDemo/myDemo.java
@@ -5,6 +5,10 @@ public class myDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("welcome java world");
+      System.out.println("demo practice");
+      System.out.println("welcome");
+      System.out.println("java coding");
 	}
+	
 
 }
